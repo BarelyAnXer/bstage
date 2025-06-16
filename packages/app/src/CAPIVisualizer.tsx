0aea67db59a9bd9f7e2f36aca5b96caa51dfa337
Bharath Nallapeta <bnallapeta@mirantis.com>
2025-06-16 13:43:57 +0530

    feat: add proxy configuration for CAPI Visualizer
    Signed-off-by: Bharath Nallapeta <bnallapeta@mirantis.com>

--- packages/app/src/CAPIVisualizer.tsx
+++ packages/app/src/CAPIVisualizer.tsx
@@ -1,15 +1,20 @@
 export const CAPIVisualizer = () => {
+  // Use backend proxy for CAPI Visualizer
+  // This works in both development and production:
+  // - Development: proxies to http://localhost:8081
+  // - Production: proxies to http://capi-visualizer.default.svc.cluster.local:8081
+  const capiVisualizerUrl = '/api/proxy/capi-visualizer/';
 
   return (
-    <>
-         <iframe
-            src="http://localhost:8081/"
-            width="100%"
-            height="820"
-            style={{ border: "1px solid black" }}
-            title="Embedded Website"
-          ></iframe>
-    </>
+    <div>
+      <iframe
+        src={capiVisualizerUrl}
+        width="100%"
+        height="820"
+        style={{ border: "1px solid black" }}
+        title="CAPI Visualizer"
+      />
+    </div>
   );
 };
 
